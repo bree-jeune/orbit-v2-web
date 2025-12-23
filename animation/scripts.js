4720b90b3e133cb377b9695305c3b54d0f6aa4ca
bree-jeune <bree.j.32@gmail.com>
2025-12-22 17:04:29 -0800

    feat: add iframe console relay for enhanced debugging

---
+++ animation/scripts.js
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/iframe-console-relay/dist/index.umd.min.js">
+  // Create a new console instance
+    const console = new IFrameConsoleRelay({
+    iframe: document.getElementById('iframe'),
+    logLevel: 'debug'
+  });
+</script>
+
