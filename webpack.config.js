c2e636444a5dc01eae80e3942517703999d0aeb6
bree-juene <bree.j.32@gmail.com>
2025-12-25 21:26:24 -0800

    feat: Add dynamic mode configuration, devtool, and historyApiFallback to webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,65 +4,71 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    clean: true,
-    publicPath: "/",
-  },
-  devServer: {
-    static: ["./dist", "./public"],
-    hot: true,
-    port: 3000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
-        },
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? false : "eval-source-map",
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      clean: true,
+      publicPath: "/",
+    },
+    devServer: {
+      static: ["./dist", "./public"],
+      hot: true,
+      port: 3000,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
         {
-          from: "public",
-          to: "",
-          globOptions: {
-            ignore: ["**/index.html"],
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader",
           },
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
         },
       ],
-    }),
-    new Dotenv({
-      systemvars: true, // Load system variables as well (e.g. from CI/CD or shell exports)
-    }),
-  ],
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "public",
+            to: "",
+            globOptions: {
+              ignore: ["**/index.html"],
+            },
+          },
+        ],
+      }),
+      new Dotenv({
+        systemvars: true, // Load system variables as well (e.g. from CI/CD or shell exports)
+      }),
+    ],
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+  };
 };
